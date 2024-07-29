342b32251206e4adc684afc6b9ccbb0ba1843687
Kevin Sun <krsun05@gmail.com>
2024-07-29 09:48:13 -0400

    Initial Commit

---
+++ Fruit_Quality/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruit_Quality
+//
+//  Created by Kevin Sun on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruit_Quality/Fruit_QualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_QualityApp.swift
+//  Fruit_Quality
+//
+//  Created by Kevin Sun on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_QualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
